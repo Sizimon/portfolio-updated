6467da660e602b55a06609b61199df2e3001df4c
Shhmon <Siizmonsus@gmail.com>
2025-08-12 17:06:30 +0200

    FEAT: Add correct meta data to fight scrapers.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const anton = Anton({
 });
 
 const workSans = Work_Sans({
-  weight: ["100","200","300", "400", "500", "600", "700"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
   variable: "--font-work-sans",
   subsets: ["latin"],
 });
@@ -24,6 +24,34 @@ const workSans = Work_Sans({
 export const metadata: Metadata = {
   title: "Szymon Samus | Portfolio",
   description: "Developer Portfolio",
+  keywords: ["Software Developer", "React", "Next.js", "TypeScript", "Portfolio"],
+  authors: [{ name: "Szymon Samus" }],
+  creator: "Szymon Samus",
+  openGraph: {
+    title: "Szymon Samus | Portfolio",
+    description: "Junior Software Developer Portfolio",
+    url: "https://szymonsamus.dev/",
+    siteName: "Szymon Samus Portfolio",
+    locale: "en_EU",
+  },
+  other: {
+    'script:ld+json': JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "name": "Szymon Samus",
+      "url": "https://szymonsamus.dev/",
+      "sameAs": [
+        "https://github.com/Sizimon",
+        "https://www.linkedin.com/in/szymon-samus-1447201a0/"
+      ],
+      "jobTitle": "Junior Software Developer",
+      "description": "Junior Software Developer with a passion for bringing ideas to life, constantly learning and contributing to innovative projects.",
+      "addressRegion": "Europe",
+    })
+  },
+  alternates: {
+    canonical: "https://szymonsamus.dev/",
+  }
 };
 
 export default function RootLayout({
@@ -36,9 +64,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${anton.variable} ${workSans.variable} antialiased`}
       >
-      <CustomThemeProvider>
-        {children}
-      </CustomThemeProvider>
+        <CustomThemeProvider>
+          {children}
+        </CustomThemeProvider>
       </body>
     </html>
   );
