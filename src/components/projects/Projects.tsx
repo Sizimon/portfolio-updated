6af9b9086809850906d8c731ff22e71ae6be467e
Szymon Samus <siizmonsus@gmail.com>
2025-08-25 22:16:55 +0200

    FIX: Links to demo broke with merge

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -39,7 +39,7 @@ const projects: Array<{
             builtWith: "Javascript, React, Node.js (Discord.js/Express), PostgreSQL",
             image: images.Grippendor ? `url(${images.Grippendor})` : "url('/images/default.jpg')",
             demo: "https://szymonsamus.dev/grippendor/",
-            github: "https://github.com/Sizimon/attendance-tracker/blob/main/README.md"
+            github: "https://github.com/Sizimon/grippendor-backend/blob/main/README.md"
         },
         {
             title: "noto()",
@@ -75,7 +75,7 @@ const projects: Array<{
             builtWith: "Javascript, React",
             image: images.Guruweather ? `url(${images.Guruweather})` : "url('/images/default.jpg')",
             demo: "https://szymonsamus.dev/guruweather/",
-            github: "https://github.com/user/project3"
+            github: "https://github.com/Sizimon/guruweather/blob/master/README.md"
         }
     ];
 
