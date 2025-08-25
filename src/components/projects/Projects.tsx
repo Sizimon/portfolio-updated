2df07711c0526a0a7b2e87fa79f7019d28bba495
Szymon Samus <siizmonsus@gmail.com>
2025-08-25 22:12:16 +0200

    FIX: Links to demo broke with merge

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -34,7 +34,7 @@ const projects: Array<{
         `,
             builtWith: "Javascript, React, Node.js (Discord.js/Express), PostgreSQL",
             image: images.Grippendor ? `url(${images.Grippendor})` : "url('/images/default.jpg')",
-            demo: "https://szymonsamus.dev/bot-dashboard/",
+            demo: "https://szymonsamus.dev/grippendor/",
             github: "https://github.com/Sizimon/attendance-tracker/blob/main/README.md"
         },
         {
@@ -61,7 +61,7 @@ const projects: Array<{
         Future plans include adding features like location-based weather alerts and a more detailed forecast view.`,
             builtWith: "Javascript, React",
             image: images.Guruweather ? `url(${images.Guruweather})` : "url('/images/default.jpg')",
-            demo: "https://szymonsamus.dev/weather-app/",
+            demo: "https://szymonsamus.dev/guruweather/",
             github: "https://github.com/user/project3"
         }
     ];
